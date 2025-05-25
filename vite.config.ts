243a974713ee9e1bcc34730c04accef1941e8ab3
Mark Visser <mjmvisser@gmail.com>
2025-05-25 13:42:14 -0400

    enable source maps for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/tubestudio/"
+  base: "/tubestudio/",
+  build: {
+    sourcemap: true // Enable sourcemaps
+  }
 })
